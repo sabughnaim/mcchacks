02d0df27f7f6f6740be22da72abec4ee897fb55b
creyes221 <carlosreyes@dhcp-10-101-70-242.wireless.northwestern.private>
2014-11-22 21:15:44 -0600

    more stuff to chat

--- js/index.js
+++ js/index.js
@@ -4,12 +4,19 @@ var io = require('socket.io')(http);
 var Parse = require('parse').Parse;
 
 Parse.initialize("J2Aoe4j2ht4F8Xz3E6uhS8NKRC9V3UpF2hjU2ToR", "lROdyJFYyYnxqcSQWeGVJQp8XBbRUNwlWTX3aSP1");
+var test = "lies lies lies ";
 
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
-});
+    var Sock = Parse.Object.extend("Sock");
+	var sock = new Sock();
+	var query = new Parse.Query(Sock);
+	sock.set("random", Math.random())
+	sock.save();
+// sock.set("socketid", test);
+// sock.save();
+// var SockCollection = Parse.Collection.extend({model: Sock});
+// var collection = new SockCollection();
+
+
 
 
 
@@ -18,11 +25,26 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+	// var id = socket.rooms;
+	// var room = id[0];
+ //    sock.set("socketid", room);
+	// sock.save();
+
   socket.on('chat message', function(msg){
-  	var room = socket.rooms;
-    io.to(room).emit('chat message', msg);
-    console.log('' + socket.rooms);
+
+    io.to(socket.rooms).emit('chat message', msg);
+    
   });
+
+  socket.on('adduser', function(room) {
+  	sock.set("socketid", room);
+	sock.save();
+  })
+
+  // socket.on('disconnect', function() {
+  // 	console.log('user gone')
+  // 	sock.destroy();
+  // });
 });
 
 http.listen(3000, function(){
